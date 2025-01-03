f63cac948285e8218d42179655f090f2f0ce3e87
Itsesp <92010091+Itsesp@users.noreply.github.com>
2025-01-03 18:47:13 +0530

    Update Largest.java

--- p1.java
+++ p1.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+public class Largest{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int largest;
+        if(a>b && a>c){
+            largest=a;
+        }else if (b>c && b>a) {
+            largest=b;
+        }else{
+            largest=c;
+        }
+        System.out.println("Largest = "+largest);
+    }
+}
